14b46c4202d9fc1e50c9630c257019496422c60b
Tarek <tarekipad@gmail.com>
2020-09-04 17:57:18 +0200

    creating contact form - js validation, php validation, email sent

--- index.php
+++ index.php
@@ -306,18 +306,83 @@ $body = "Mail function in PHP";
 
 $headers = "From: xyz@gmail.com";
 
-// this returns a 1 or a 0 if successfull
-
-
-
+// this returns a 1 or a 0 if successfull so put the mail function in an if statement. if true then successfull message appears
 if (mail($emailTo, $subject, $body, $headers)) {
 
     echo "Email is off!";
 } else {
     echo "email cannot be sent for some reason";
 }
-
-
-
-
+echo "<br><br>";
+echo "<br><br>";
+echo "<br><br>";
+echo "<br><br>";
 ?>
+
+
+
+
+
+
+<!-- 
+1. Contact form
+2. JS/Jquery Validation
+3. PHP server side validation
+4. Check that email is sent
+ -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Contact Form PHP</title>
+</head>
+
+<body>
+
+    <header class="container">
+        <h2>Get in Touch</h2>
+    </header>
+
+
+    <div class="container mt-5">
+
+        <form action="" method="POST">
+
+            <div class="form-group">
+                <label for="">Email Address</label>
+                <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId" require>
+                <!-- <small id="helpId" class="text-muted">Help text</small> -->
+            </div>
+
+            <div class="form-group">
+                <label for="">Subject</label>
+                <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId" require>
+                <!-- <small id="helpId" class="text-muted">Help text</small> -->
+            </div>
+
+            <div class="form-group">
+                <label for="">Message:</label>
+                <input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId" require>
+                <!-- <small id="helpId" class="text-muted">Help text</small> -->
+            </div>
+
+            <button type="submit" class="btn btn-primary">SEND</button>
+        </form>
+
+
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+
+</html>
